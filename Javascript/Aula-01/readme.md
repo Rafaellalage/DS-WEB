47f1b194b4425e1e22dc31ee64b85b0cbc68454d
Rafaellalage <rafaella@lage.com.br>
2025-01-31 12:23:33 -0300

    Primeiro commit 2025

---
+++ Javascript/Aula-01/readme.md
@@ -0,0 +1,4 @@
+Aula Introdutoria sobre Lógica de Programação com Javascript
+
+Declaração de variaveis
+Alert, Confirm, Prompt, Adição
---
+++ Javascript/Aula-01/script.js
@@ -0,0 +1,23 @@
+console.log("Olá Mundo");
+
+//Exemplo de alert()
+    //alert("Olá Mundo 1")
+
+//Exemplo de confirm()
+    //var teste = confirm ("Olá Mundo 2")
+
+    //Exibindo a opcao escolhida pelo usuário
+    //console.log(teste)
+
+//Exemplo de Prompt()
+    //var testePrompt = prompt ("Digite o seu nome:")    
+    //Exibindo  o nome do usuário
+    //console.log(testePrompt)
+    //alert(testePrompt)
+
+// Tentativa de Adição
+    var numero1 = Number.parseInt(prompt("Digite o primeiro numero:"))
+    var numero2 = Number.parseInt(prompt("Digite o segundo numero:"))
+    //Verificando o tipo da variavel
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
